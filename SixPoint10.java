d95739d0e66d30d585e27d46456c7a7b84de86f8
KrisHarrison <KrisHarrison143@gmail.com>
2018-09-19 22:40:24 -0400

    Method Exercises

---
+++ SixPoint10.java
@@ -0,0 +1,22 @@
+public class SixPoint10 {
+    public static void main(String [] args){
+        displayPrimes();
+    }
+    public static void displayPrimes(){
+            System.out.print("All prime numbers under 10000");
+        for(int i = 1; i < 10000; i++){
+            if( isPrime(i)){
+                System.out.printf("\n %d", i);
+            }
+        }
+    }
+    public static boolean isPrime(int number){
+
+        for(int divisor = 2; divisor < number; divisor++){
+            if(number % divisor == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- SixPoint9.java
+++ SixPoint9.java
@@ -1,5 +1,24 @@
 public class SixPoint9 {
     public static void main(String [] args){
-        
+
+        displayResult();
+    }
+    public static void displayResult(){
+        double metres = 20;
+
+        System.out.print("Feet\t\tMetres\t\t|\t\tMetres\t\t\tFeet");
+        System.out.print("\n_________________________________________________________");
+
+        for(double foot = 1.0; foot <= 10; foot++){
+            System.out.printf("\n%.0f\t\t\t%.3f\t\t|\t\t%.1f\t\t\t%.3f",foot,footToMetres(foot),metres,metresToFoot(metres));
+
+            metres+=5;
+        }
+    }
+    public static double footToMetres(double foot){
+        return 0.305 * foot;
+    }
+    public static double metresToFoot(double metres){
+        return 3.279 * metres;
     }
 }
