7bbec8165b3eba0ae2caf09fbc26a6aaf1e1d7b5
KrisHarrison <KrisHarrison143@gmail.com>
2018-09-25 00:59:18 -0400

    Method exercises

--- SixPoint13.java
+++ SixPoint13.java
@@ -1,5 +1,25 @@
 public class SixPoint13 {
     public static void main(String [] args){
-        
+
+        displayM();
+    }
+    public static void displayM(){
+        System.out.print("i\t\t\tm(i)");
+        double numerator = 1;
+        double denominator = 2;
+        double result = 0;
+        double quotient = 0;
+
+        for(int i = 1; i <= 20; i++){
+
+            result += calcM(numerator,denominator);
+            System.out.printf("\n%d\t\t\t%.4f",i,result);
+            numerator ++;
+            denominator++;
+        }
+    }
+    public static double calcM(double numerator, double denominator){
+
+        return numerator / denominator;
     }
 }
---
+++ SixPoint14.java
@@ -0,0 +1,5 @@
+public class SixPoint14 {
+    public static void main(String [] args){
+        
+    }
+}
