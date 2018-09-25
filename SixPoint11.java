7300631d155735f191828a55552a7cbcc69ffcaa
KrisHarrison <KrisHarrison143@gmail.com>
2018-09-24 23:14:52 -0400

    Commision exercise

--- SixPoint11.java
+++ SixPoint11.java
@@ -1,5 +1,29 @@
 public class SixPoint11 {
     public static void main(String [] args){
 
+        displayCommissionRate();
+    }
+    public static void displayCommissionRate(){
+            System.out.print("Sales Amount\t\tCommision" + "\n______________________________");
+        for(int i = 10000; i <= 100000; i+=5000){
+
+
+            System.out.printf("\n\t%d\t\t\t%.2f",i,computeCommision(i));
+        }
+    }
+    public static double computeCommision(double salesAmount){
+
+        double commision = 0;
+
+        if(salesAmount > 0.01 || salesAmount < 5000){
+            commision += (salesAmount * 0.08);
+        }
+        else if(salesAmount > 5000.01 && salesAmount < 10000){
+            commision += (salesAmount * 0.10);
+        }
+        else if(salesAmount > 10000.01){
+            commision += (salesAmount * 0.12);
+        }
+        return commision;
     }
 }
