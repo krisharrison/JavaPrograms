78f529fa95e1a2f0e94992559b776f472a774400
KrisHarrison <33849103+KrisHarrison@users.noreply.github.com>
2018-10-16 00:10:04 -0400

    Update and rename SixPoint1.java to PentagonalNumbers .java

--- PentagonalNumbers .java
+++ PentagonalNumbers .java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SixPoint1 {
+public class PentagonalNumbers{
     public static void main(String [] args) {
 
         Scanner input = new Scanner(System.in);
