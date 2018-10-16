ad95d5b7a7fa5b48446e86064f7d7f16dbb6e484
KrisHarrison <33849103+KrisHarrison@users.noreply.github.com>
2018-10-16 00:46:20 -0400

    Update and rename SixPoint17.java to Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class SixPoint17 {
+public class Matrix{
     public static void main(String [] args){
 
         Scanner input = new Scanner(System.in);
