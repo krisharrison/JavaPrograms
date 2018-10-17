3508be677379f1600c268739a264e279521fa727
KrisHarrison <KrisHarrison143@gmail.com>
2018-10-17 16:40:07 -0400

    Java programs

---
+++ Letters.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+
+public class Letters {
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);
+
+        while(true) {
+            try {
+                System.out.print("\nEnter a word: ");//gain input from user
+                String word = input.next();//Store input into word
+
+                if (isValid(word) == false) {//if word is not valid
+                    System.out.print("\nPlease enter a valid word");//Display error message
+                }//End If
+                else {//Else
+                    System.out.printf("\nThe number of letters in %s: %d", word, numOfChars(word));//Display results
+                    break;
+                }//End Else
+            } catch (Exception e) {//Catch
+                System.out.print("\nPlease enter a valid String");//Display error message
+                input.next();
+            }
+        }
+
+    }
+
+    /**
+     * count the number of letters in a word
+     * @param word
+     * @return int
+     */
+    public static int numOfChars(String word){
+        return word.length();//Return word length
+    }
+
+    /**
+     * Check if word is valid
+     * @param word
+     * @return
+     */
+    public static boolean isValid(String word){
+        //Array of invalid characters for user input
+        char [] invalidChars = {0,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126,127};
+        boolean isValid = true;//Init isValid to true
+
+        for(int index = 0; index < word.length(); index++){//Cycle through each character of word
+            for(int invalidIndex = 0; invalidIndex < invalidChars.length; invalidIndex++){//Cycle through each element of invalid characters
+
+                if(word.charAt(index) == invalidChars[invalidIndex]){//If a character from user input is equal to any element of invalidChars
+                    isValid = false;//init isValid to false
+                }
+            }
+        }
+
+        return isValid;//return isValid
+    }
+
+}
--- MyTriangle.java
+++ MyTriangle.java
@@ -7,38 +7,57 @@ public class MyTriangle {
 
         while(true){
             try {
-                System.out.print("\nPlease enter the length of three sides of a triangle: ");
-                double side1 = input.nextDouble();
-                double side2 = input.nextDouble();
-                double side3 = input.nextDouble();
+                System.out.print("\nPlease enter the length of three sides of a triangle: "); //Get input from user
+                double side1 = input.nextDouble();//Side1
+                double side2 = input.nextDouble();//Side2
+                double side3 = input.nextDouble();//Side3
 
-                if(isValid(side1,side2,side3)){
-                    System.out.printf("%n The area of the triangle is: %.2f", calcArea(side1,side2,side3));
-                }
-                else{
-                    System.out.println("\nPlease enter in lengths for a a valid triangle. E.g. ");
-                }
+                if(isValid(side1,side2,side3)){ //Triangle is valid is true
+                    System.out.printf("%n The area of the triangle is: %.2f", calcArea(side1,side2,side3)); //Display area of triangle
+                    break;
+                }//End If
+                else{//Else
+                    System.out.println("\nPlease enter in lengths for a a valid triangle. E.g. ");//Error message
+                }//End Else
 
             }
-            catch(Exception e){
-                System.out.println("\n Please enter a valid number for each side of the triangle: ");
+            catch(Exception e){//Catch
+                System.out.println("\n Please enter a valid number for each side of the triangle: ");//Error Message
                 input.next();
             }
         }
 
 
 
+
     }
+
+    /**
+     * Check if triangle is valid
+     * Triangle is valid if the sum of the first two sides is greate than the third
+     * @param side1
+     * @param side2
+     * @param side3
+     * @return boolean
+     */
     public static boolean isValid(double side1, double side2, double side3){
 
         boolean isValid = false;
 
-        if(((side1 + side2) > side3) || ((side1 + side3) > side2) || ((side2 + side3) > side1)){
+        if((side1 + side2) > side3){
             isValid = true;
         }
 
         return isValid;
     }
+
+    /**
+     * Computes the area of the triangle
+     * @param side1
+     * @param side2
+     * @param side3
+     * @return double
+     */
     public static double calcArea(double side1, double side2, double side3){
 
         double s = (side1 + side2 + side3) / 2;
