14933f198701b5fe75aa33adf1efbb01b47745a1
KrisHarrison <KrisHarrison143@gmail.com>
2018-10-20 23:05:24 -0400

    Java promgram

---
+++ BabylonianMethod.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BabylonianMethod {
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);
+
+    }
+    public static double sqrt(long n){
+        double sqrt = 0.0;
+
+        return sqrt;
+    }
+}
