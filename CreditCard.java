fbfbf011c1281de5d7796dbf7d976ede08e44131
KrisHarrison <KrisHarrison143@gmail.com>
2018-11-16 23:20:16 -0500

    Java programs

--- CreditCard.java
+++ CreditCard.java
@@ -1,4 +1,71 @@
-package PACKAGE_NAME;
-
 public class CreditCard {
+    public static void main(String [] args){
+
+    }
+
+    /**
+     * Return true is card number is valid
+     * @param number
+     * @return
+     */
+    public static boolean isValid(int number){
+
+    }
+
+    /**
+     * Sum of even number placed numbers
+     * @param number
+     * @return
+     */
+    public static int sumOfDoubleEvenPlace(int number){
+
+    }
+
+    /**
+     *Return single number digit. If number it's two digits add to get a single digit
+     * @param number
+     * @return
+     */
+    public static int getDigit(int number){
+
+    }
+
+    /**
+     *Return the sum of odd place digits
+     * @param number
+     * @return
+     */
+    public static int sumOfOddPlace(int number){
+
+    }
+
+    /**
+     *Return true if digit d is a prefix for number
+     * @param number
+     * @param d
+     * @return
+     */
+    public static boolean prefixMatched(int number, int d){
+
+    }
+
+    /**
+     *Return the number of digits
+     * @param d
+     * @return
+     */
+    public static int getSize(int d){
+
+    }
+
+    /**
+     *Return the first k number of digits from number. If the
+     * number of digits in number is less than k, return number.
+     * @param number
+     * @param k
+     * @return
+     */
+    public static int getPrefix(int number, int k){
+
+    }
 }
