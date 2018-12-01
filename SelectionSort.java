c4d9de4aa9ae8f31128ae5a8a7cf187471f747a4
KrisHarrison <KrisHarrison143@gmail.com>
2018-12-01 16:10:47 -0500

    Java Programs

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,36 +1,36 @@
 public class SelectionSort {
     public static void main(String [] args){
 
-        int array[] = {3,8,5,12,50,6,2};
+        int array[] = {3,8,5,12,50,6,2};//Init array with size of 7
 
-        for(int element:selectionSort(array))
-            System.out.print(element + " ");
+        selectionSort(array);
+
+        for(int element:array)//Process sorted array
+            System.out.print(element + " ");//Display sorted array
 
     }
-    public static int[] selectionSort(int array[]){
-        int firstIndex = 0;
-        int last = array[array.length - 1];
-        int smallestNum = array[0];
-        int swappedIndex = 0;
-        int temp;
 
-       for(int i = 0; i < array.length; i++){
-            for(int index = 0; index < array.length;index++){
-                if(array[index] < smallestNum) {
-                    smallestNum = array[index];
-                    swappedIndex = index;
+    /**
+     * Sorts array using selection sort
+     * @param array
+     * @return int array
+     */
+    public static void selectionSort(int array[]){
+
+       for(int firstElement = 0; firstElement < array.length - 1; firstElement++){//First element
+                    int  minNum = array[firstElement];
+                    int minNumIndex = firstElement;
+            for(int index = firstElement + 1; index < array.length;index++){
+                if(array[index] < minNum) {
+                    minNum = array[index];
+                    minNumIndex = index;
                 }
-
-                firstIndex++;
             }
 
-            temp = array[firstIndex];
-            array[firstIndex] = smallestNum;
-            array[swappedIndex] = temp;
+            if(minNumIndex != firstElement)//swap elements if necessary
+                array[minNumIndex] = array[firstElement];
+                array[firstElement] = minNum;
+        }//end outer for
 
-            firstIndex = 0;
-        }
-
-        return array;
     }
 }
