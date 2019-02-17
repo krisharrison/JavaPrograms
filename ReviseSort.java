39cbdc8946c79740782e8406f9fd5cc453fecbdb
KrisHarrison <KrisHarrison143@gmail.com>
2019-02-17 04:05:05 -0500

    jva program

---
+++ ReviseSort.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class ReviseSort {
+    public static void main(String [] args){
+
+        Scanner input = new Scanner(System.in);//Scanner
+        double [] list = new double[10];//list array of double
+        int k = 0;//k = 0;
+        int n = list.length;//n = 10
+        double userNum;//userNum
+        //2.3 -10 60.4 22.1 19.0 210.12 92.9 189.2 43.89 -42.39
+
+        System.out.print("Enter 10 digits: ");
+
+        while(k < n){//While k < n
+            try{//Try
+
+                userNum = input.nextDouble();//Store user input in userNum
+                list[k] = userNum;//Insert userNum in array
+
+                k++;//Increment by 1
+            }//End try
+            catch(Exception e){//Catch
+                System.out.print("\nError!Number must be a double value: ");//Display error message
+                input.next();//next line
+            }//End catch
+        }//End while
+
+        displayResults(list);
+        //sortAscending(list);
+
+
+    }
+
+    /**
+     * Display results
+     * @param list
+     */
+    public static void displayResults(double...list){
+        sortAscending(list);
+
+        System.out.println("\n\nSorted Array: ");
+        for(double e:list)
+            System.out.printf("%.2f\t", e);
+
+    }
+    /**
+     * Sort array in ascending order using selection sort
+     * @param list
+     */
+    public static void sortAscending(double...list){
+
+        for(int i = list.length-1;i >= 0;i--){
+            double currentMax = list[i];//Current max character
+            int currentMaxIndex = i;//Current index
+
+            for(int j = i-1;j>=0; j--){
+                if(list[j] > currentMax) {//if list @ index j > currentMax
+                    currentMax = list[j];//Store list[j] to currentMax
+                    currentMaxIndex = j;//currentMaxIndex = j
+                }
+
+            }
+            //Swap largest number with the last digit
+            if(currentMaxIndex != i)
+                list[currentMaxIndex] = list[i];
+                list[i] = currentMax;
+            //System.out.print(list[i] + " ");
+        }
+    }
+}
---
+++ Sorted.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Sorted {
+    public static void main(String [] args) {
+
+        Scanner input = new Scanner(System.in);//Scanner
+        int numOfNums;//size of array
+
+        System.out.print("Enter list: ");//Display message
+
+        while (true) {//while true
+            try {
+                numOfNums = input.nextInt();//gain input from user
+
+                //If number is valid
+                if(isValid(numOfNums))
+                    break;//Break
+                else
+                    System.out.println("\nError!Number entered must be greater than 0\nEnter List");//Error message
+
+
+            } catch (Exception e) {
+                System.out.println("\nError!Number must be an integer\nEnter list: ");//Error message
+                input.next();//Next line
+            }
+        }//End while
+
+        int list[] = new int[numOfNums];//array call list
+        int i = 0;//Index
+        int userNum;//userNum
+
+        while (i < list.length){//While i < list.length
+            try {
+
+                userNum = input.nextInt();//User input
+                list[i] = userNum;//Insert element in array
+
+                i++;
+            } catch (Exception e) {//Catch
+                System.out.println("Error!\nEnter list: ");//Error message
+                input.next();//Next Line
+            }//End catch
+        }//End while
+
+        displayResults(list);
+    }
+
+    public static void displayResults(int...list){
+        if(isSorted(list))
+            System.out.println("\nThis list is sorted!");
+        else
+            System.out.println("\nThis list is not sorted!");
+    }
+    /**
+     * Determines whether array is sorted
+     * @param list
+     * @return
+     */
+
+    public static boolean isSorted(int [] list){
+
+        int front = 0;//Front = 0
+        int back = list.length - 1;//Back = list.length - 1
+
+        while(front <= back){//while front <=  back
+
+            //Determine whether elements are sorts in ascending order
+            if(list[front] > list[front+1] || list[back-1] > list[back]){
+                return false;//Return false
+            }
+
+
+            front++;//increment from by 1
+            back--;//decrement back by 1
+        }
+        return true;
+    }
+
+    /**
+     * Number is valid when greater than 0
+     * @param num
+     * @return
+     */
+    public static boolean isValid(int num){
+        if(num > 0)
+            return true;
+
+        return false;
+    }
+}
